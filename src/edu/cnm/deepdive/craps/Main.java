7ff9ae52810d19e991030ad3008f7abb83585ec3
Matthew Morgan <mmorgan55@cnm.edu>
2018-10-18 10:59:31 -0600

    Added logic for the dice to play the craps game and added shortcut keys for playing in the UI.

--- src/edu/cnm/deepdive/craps/Main.java
+++ src/edu/cnm/deepdive/craps/Main.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.craps;
 
+import edu.cnm.deepdive.craps.controller.Controller;
 import java.io.IOException;
 import java.util.ResourceBundle;
 import javafx.application.Application;
@@ -10,9 +11,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  private static final String RESOURCE_PATH = "res/ui";
+  private static final String MAIN_RESOURCE = "res/main.fxml";
+  private static final String WINDOW_TITLE_KEY = "window_title";
+
   private ClassLoader classLoader;
   private ResourceBundle bundle;
   private FXMLLoader fxmlLoader;
+  private Controller controller;
 
   public static void main(String[] args) {
     launch(args);
@@ -24,21 +30,27 @@ public class Main extends Application {
     setupStage(stage, loadLayout());
   }
 
+  @Override
+  public void stop() throws Exception {
+    controller.stop();
+    super.stop();
+  }
+
   private void setupLoaders() {
     classLoader = getClass().getClassLoader();
-    bundle = ResourceBundle.getBundle("res/ui");
-    fxmlLoader = new FXMLLoader(classLoader.getResource("res/main.fxml"), bundle);
+    bundle = ResourceBundle.getBundle(RESOURCE_PATH);
+    fxmlLoader = new FXMLLoader(classLoader.getResource(MAIN_RESOURCE), bundle);
   }
 
   private Parent loadLayout() throws IOException {
     Parent root = fxmlLoader.load();
-    // TODO DO something more??
+    controller = fxmlLoader.getController();
     return root;
   }
 
   private void setupStage(Stage stage, Parent root) {
     Scene scene = new Scene(root);
-    stage.setTitle(bundle.getString("window_title"));
+    stage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
     // TODO Set icon, etc.
     stage.setResizable(false);
     stage.setScene(scene);
